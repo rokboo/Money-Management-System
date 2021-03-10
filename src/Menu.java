92e27b38b296495b379a31fb8793bc1adf718bff
rokboo <rokboo@naver.com>
2021-03-10 16:08:15 +0900

    added menu.java

---
+++ src/Menu.java
@@ -0,0 +1,12 @@
+
+public class Menu {
+
+	public static void main(String[] args) {
+		System.out.println("1. My Income");
+		System.out.println("2. My Expenditure");
+		System.out.println("3. Edit Income or Expenditure");
+		System.out.println("4. View Property");
+		System.out.println("5. Exit");
+	}
+
+}
