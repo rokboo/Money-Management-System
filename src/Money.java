a7783412eceee891e24fd305c39bd6023083d1ac
rokboo <80380681+rokboo@users.noreply.github.com>
2021-05-11 14:12:27 +0900

    upadate abstract and interface

--- src/Money.java
+++ src/Money.java
@@ -1,26 +1,33 @@
-public class Money {
-		private int money;
-		private String name;
-		public int getMoney() {
-			return money;
-		}
-		public void setMoney(int money) {
-			this.money = money;
-		}
-		public String getName() {
-			return name;
-		}
-		public void setName(String name) {
-			this.name = name;
-		}
-		public Money() {
-			
-		}
-		public Money(int money, String name) {
-			this.setMoney(money);
-			this.setName(name);
-		}
-		public void Print() {
-			System.out.print(this.getName() + " : " + this.getMoney());
-		}
+abstract public class Money implements getinformation{
+	private int money;
+	private String name;
+	private double interest;	
+	private int month;
+	public int getMoney() {
+		return money;
+	}
+	public void setMoney(int money) {
+		this.money=money;		
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name=name;
+	}
+	public int getMonth() {
+		return month;
+	}
+	public void setMonth(int month) {
+		this.month=month;
+	}
+	public double getInterest() {
+		return interest;
+	}
+	public void setInterest(double interest) {
+		this.interest = interest;
+	}
+	public Money() {}
+		public abstract void Print();
+		public abstract int calinterest();
 	}
--- src/Saving.java
+++ src/Saving.java
@@ -1,32 +1,17 @@
 public class Saving extends Money{
-	private int saving_interest;
-	private int saving_month;
-	public int getSaving_interest() {
-		return saving_interest;
-	}
-	public void setSaving_interest(int saving_interest) {
-		this.saving_interest = saving_interest;
-	}
-	public int getSaving_month() {
-		return saving_month;
-	}
-	public void setSaving_month(int saving_month) {
-		this.saving_month = saving_month;
-	}
-	public Saving(int money, String name, int saving_interest, int saving_month) {
+	public Saving(int money, String name, double interest, int month) {
 		this.setMoney(money);
 		this.setName(name);
-		this.setSaving_interest(saving_interest);
-		this.setSaving_month(saving_month);
-	}
-	public int saving_income(int money, int saving_interest, int saving_month) {
-		int income = 0;
-		income = money*saving_month*saving_interest/100;
-		return income;
+		this.setInterest(interest);
+		this.setMonth(month);
 	}
 	public void Print() {
 		System.out.print(this.getName() + " : " + this.getMoney());
-		System.out.println(" saving interest : " + this.getSaving_interest() + " passed months : " + this.getSaving_month());;
-	
+		System.out.println(" 적금이자율 : " + this.getInterest() + " 적금기간 : " + this.getMonth());;
+		}
+	public int calinterest() {
+		int income = 0;
+		income = (int)(getMoney()*getMonth()/12*getInterest()/100);
+		return income;
 	}
 }
