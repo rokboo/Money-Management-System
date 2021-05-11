c29ef40e5c468a22e3c2895ed9dfd5c0ac7b3b66
rokboo <80380681+rokboo@users.noreply.github.com>
2021-05-11 14:06:44 +0900

    abstract and interface

--- src/Loan.java
+++ src/Loan.java
@@ -1,31 +1,17 @@
 public class Loan extends Money {
-	private int interest;	
-	private int Loan_month;
-	public int getLoan_month() {
-		return Loan_month;
-	}
-	public void setLoan_month(int Loan_month) {
-		this.Loan_month=Loan_month;
-	}
-	public int getInterest() {
-		return interest;
-	}
-	public void setInterest(int interest) {
-		this.interest = interest;
-	}
-	public Loan(int money, String name, int interest, int Loan_month) {
+	public Loan(int money, String name, double interest, int month) {
 		this.setMoney(money);
 		this.setName(name);
 		this.setInterest(interest);
-		this.setLoan_month(Loan_month);
+		this.setMonth(month);
 	}
-	public int debt(int money, int interest, int Loan_month) {
+	public int calinterest() {
 		int debt = 0;
-		debt = money*interest*Loan_month/100;
+		debt = (int)(getMoney()*getMonth()/12*getInterest()/100);
 		return debt;
 	}
 	public void Print() {
 		System.out.print(this.getName() + " : " + this.getMoney());
-		System.out.println(" loan interest : " + this.getInterest() + " passed months : " + this.getLoan_month());
+		System.out.println(" 대출이자율 : " + this.getInterest() + " 대출기간 : " + this.getMonth());
 	}
 }
--- src/MoneyManage.java
+++ src/MoneyManage.java
@@ -2,9 +2,9 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MoneyManage {
-	ArrayList<Money> income = new ArrayList<Money>();
+	ArrayList<Income> income = new ArrayList<Income>();
 	ArrayList<Loan> Loan = new ArrayList<Loan>();
-	ArrayList<Money> expend = new ArrayList<Money>();
+	ArrayList<Expend> expend = new ArrayList<Expend>();
 	ArrayList<Saving> Saving = new ArrayList<Saving>();
 	Scanner input;
 	MoneyManage(Scanner input){
@@ -12,59 +12,59 @@ public class MoneyManage {
 	}
 	
 	public void income() {
-		System.out.print("Did you get a loan?");
+		System.out.print("대출을 받았습니까?");
 		String ans = input.next();
-		if(ans.equals("yes")) {
-			System.out.print("Enter your loan money : ");
+		if(ans.equals("네")) {
+			System.out.print("대출금액을 입력하십시오 : ");
 			int m = input.nextInt();
 			String n = "대출";
-			System.out.print("Enter loan interst : ");
-			int i = input.nextInt();
-			System.out.println("How many months have passed?");
+			System.out.print("대출이자율를 입력하십시오 : ");
+			double i = input.nextDouble();
+			System.out.println("대출기간이 얼마나 되었습니까?");
 			int month = input.nextInt();
 			Loan loan = new Loan(m,n,i,month);
-			Money debt = new Money(loan.debt(m,i,month),"대출이자");
+			Expend debt = new Expend(loan.calinterest(),"대출이자");
 			Loan.add(loan);
 			expend.add(debt);
 		}
-		else if(ans.equals("no")) {
-			System.out.print("Enter your income : ");
+		else if(ans.equals("아니요")) {
+			System.out.print("수입금액을 입력하십시오 : ");
 			int m = input.nextInt();
-			System.out.print("Enter your income name : ");
+			System.out.print("수입원을 입력하십시오 : ");
 			String n = input.next();
-			Money money = new Money(m,n);
+			Income money = new Income(m,n);
 			income.add(money);			
 		}
 		else {
-			System.err.print("You wrote wrong answer");
+			System.err.print("잘못된 입력입니다.");
 		}
 	}	
 	public void expend() {		
-		System.out.print("Did you save a money?");
+		System.out.print("적금을 들었습니까?");
 		String ans = input.next();
-		if(ans.equals("yes")) {
-			System.out.print("Enter your saving money : ");
+		if(ans.equals("네")) {
+			System.out.print("적금액을 입력하십시오 : ");
 			int m = input.nextInt();
 			String n = "적금";
-			System.out.print("Enter saving interst : ");
-			int i = input.nextInt();
-			System.out.println("How many months have passed?");
+			System.out.print("적금이자율를 입력하십시오 : ");
+			double i = input.nextDouble();
+			System.out.println("적금기간이 얼마나 되었습니까?");
 			int month = input.nextInt();
 			Saving saving = new Saving(m*month,n,i,month);
-			Money interest =new Money(saving.saving_income(m,i,month),"적금이자");
+			Income interest =new Income(saving.calinterest(),"적금이자");
 			Saving.add(saving);
 			income.add(interest);
 		}
-		else if(ans.equals("no")) {
-			System.out.print("Enter your expenditure : ");
+		else if(ans.equals("아니요")) {
+			System.out.print("지출금액을 입력하십시오 : ");
 			int m = input.nextInt();
-			System.out.print("Enter your expenditure name : ");
+			System.out.print("지출원을 입력하십시오 : ");
 			String n = input.next();
-			Money money = new Money(m,n);
+			Expend money = new Expend(m,n);
 			expend.add(money);
 		}
 		else {
-			System.err.print("You wrote wrong answer");
+			System.err.print("잘못된 입력입니다");
 		}
 	}
 	
@@ -84,101 +84,99 @@ public class MoneyManage {
 		for(int i =0 ; i < Saving.size() ; i++) {
 			Saving.get(i).Print();
 		}		
-		System.out.println("Which one do you want to edit?(Enter income, expend, loan, saving and number)");
+		System.out.println("수정하고싶은 목록을 입력하십시오(수입,대출,지출,적금 중 하나를 입력)");
 		String ans = input.next();
-		System.out.println("Enter the number)");
+		System.out.println("몇번째 항목을 수정할지 입력하십시오");
 		int i = input.nextInt() - 1;
-		if(ans.equals("income")) {
+		if(ans.equals("수입")) {
 			if((income.get(i).getName().equals("적금이자"))){
-				System.out.println("If you want to edit saving income, you need to edit Saving");
+				System.out.println("적금이자를 수정하려면 적금금액을 수정해주십시오");
 			}
 			else {
-				System.out.print("Enter your income : ");
+				System.out.print("수정할 수입금액을 입력하십시오 : ");
 				int money = input.nextInt();
 				income.get(i).setMoney(money);
-				System.out.print("Enter your income name : ");
+				System.out.print("수정할 수입원을 입력하십시오 : ");
 				String name = input.next();
 				income.get(i).setName(name);
 			}
 		}
-		else if(ans.equals("loan")) {
-			System.out.print("Enter your loan money : ");
+		else if(ans.equals("대출")) {
+			System.out.print("수정할 대출금액을 입력하십시오 : ");
 			int money = input.nextInt();
 			income.get(i).setMoney(money);
-			System.out.print("Enter your loan interest : ");
-			int interest = input.nextInt();
-			System.out.println("How many months have passed?");
+			System.out.print("수정할 대출이자율를 입력하십시오 : ");
+			double interest = input.nextDouble();
+			System.out.println("수정할 대출기간을 입력하십시오");
 			int month = input.nextInt();
 			Loan.get(i).setMoney(money);
 			Loan.get(i).setInterest(interest);
-			Loan.get(i).setLoan_month(month);
-			int debt = money*interest*month/100;
+			Loan.get(i).setMonth(month);
 			for(int j = 0 ; j < expend.size() ; j++) {
 				if(expend.get(j).getName().equals("대출이자")) {
-					expend.get(j).setMoney(debt);
+					expend.get(j).setMoney(Loan.get(i).calinterest());
 				}
 			}
 			
 		}
-		else if(ans.equals("expend")) {
+		else if(ans.equals("지출")) {
 			if((expend.get(i).getName().equals("대출이자"))) {
-				System.out.println("If you want to edit saving income, you need to edit Loan");
+				System.out.println("대출이자를 수정하려면 대출금액을 수정해주십시오");
 			}
 			else{
-				System.out.print("Enter your expenditure : ");
+				System.out.print("수정할 지출금액을 입력하십시오 : ");
 				int money = input.nextInt();
 				expend.get(i).setMoney(money);
-				System.out.print("Enter your expenditure name : ");
+				System.out.print("수정할 지출원을 입력하십시오 : ");
 				String name = input.next();
 				expend.get(i).setName(name);
 			}
 		}	
-		else if(ans.equals("saving")) {
-			System.out.print("Enter your saving money : ");
+		else if(ans.equals("적금")) {
+			System.out.print("수정할 적금금액을 입력하십시오 : ");
 			int money = input.nextInt();
 			income.get(i).setMoney(money);
-			System.out.print("Enter your saving interest : ");
-			int interest = input.nextInt();
-			System.out.println("How many months have passed?");
+			System.out.print("수정할 적금이자율을 입력하십시오 : ");
+			double interest = input.nextDouble();
+			System.out.println("수정할 적금기간을 입력하십시오");
 			int month = input.nextInt();
-			Saving.get(i).setMoney(money);
-			Saving.get(i).setSaving_interest(interest);
-			Saving.get(i).setSaving_month(month);
-			int debt = money*interest*month/100;
+			Saving.get(i).setMoney(money*month);
+			Saving.get(i).setInterest(interest);
+			Saving.get(i).setMonth(month);
 			for(int j = 0 ; j < income.size() ; j++) {
 				if(income.get(j).getName().equals("적금이자")) {
-					income.get(j).setMoney(debt);
+					income.get(j).setMoney(Saving.get(i).calinterest());
 				}
 			}
 		}
 	}
 	
 	public void view() {
-		System.out.print("What do you want to view?");
+		System.out.print("어떤 목록을 확인하시겠습니까?(수입,대출,지출,적금,모두 중 하나를 입력)");
 		String v = input.next();
-		if(v.equals("income")) {
+		if(v.equals("수입")) {
 			for(int i =0 ; i < income.size() ; i++) {
 				income.get(i).Print();
 			}
 			System.out.println();
 		}
-		else if(v.equals("loan")) {
+		else if(v.equals("대출")) {
 			for(int i =0 ; i < Loan.size() ; i++) {
 				Loan.get(i).Print();
 			}
 		}
-		else if(v.equals("expend")) {
+		else if(v.equals("지출")) {
 			for(int i =0 ; i < expend.size() ; i++) {
 				expend.get(i).Print();
 			}
 			System.out.println();
 		}
-		else if(v.equals("saving")){
+		else if(v.equals("적금")){
 			for(int i =0 ; i < Saving.size() ; i++) {
 				Saving.get(i).Print();
 			}	
 		}
-		else if(v.equals("all")) {
+		else if(v.equals("모두")) {
 			for(int i = 0 ; i < income.size() ; i++) {
 				income.get(i).Print();
 			}
@@ -196,6 +194,6 @@ public class MoneyManage {
 		}
 	}		
 	public void Exit() {
-		System.out.println("Exit program");
+		System.out.println("프로그램을 종료합니다");
 	}
 }
